18b46fb8396e0c34fa196dcd8ce9c6d475c21bab
Jesus Hernandez <jesus@jesuscodes.dev>
2026-03-22 02:09:43 -0700

    Random obstacle heights, need to fix gap being too small

--- Assets/Scripts/ObstacleStructureManager.cs
+++ Assets/Scripts/ObstacleStructureManager.cs
@@ -1,14 +1,35 @@
+using Unity.VectorGraphics;
 using UnityEngine;
 
 public class ObstacleStructureManager : MonoBehaviour
 {
     [SerializeField] private GameObject topObstacle;
     [SerializeField] private GameObject bottomObstacle;
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject ground;
+
+    private float sceneHeight;
+    private float groundHeight = 1f; //used to make sure obstacels spawn above ground. Will need to manually change but doubt I will want the ground to be any taller
+    private float obsGap = 2f; //gap the player will need to jump through
+    private float minObsSize = 2f; //currently want the smallest a obs can be to be 2 units
+    
+    //the largest a obs could be accounting for all other constraints. Will be used to randomly generate obs height
+    private float maxObsSize; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        topObstacle.transform.position = new Vector2(0, 3f);
-        bottomObstacle.transform.position = new Vector2(0f, -3f);
+        sceneHeight = cam.orthographicSize * 2;
+
+        maxObsSize = sceneHeight - obsGap - minObsSize - groundHeight;
+
+        float bottomObsHeight = Random.Range(minObsSize, maxObsSize);
+        bottomObstacle.transform.localScale = new Vector2(1, bottomObsHeight);
+        bottomObstacle.transform.position = new Vector2(0, ground.transform.position.y + (bottomObsHeight / 2) + (groundHeight / 2));
+
+        float topObsHeight = sceneHeight - obsGap - bottomObsHeight;
+        topObstacle.transform.localScale = new Vector2(1, topObsHeight);
+        topObstacle.transform.position = new Vector2(0, (sceneHeight / 2) - (topObsHeight / 2));
     }
 
     // Update is called once per frame
